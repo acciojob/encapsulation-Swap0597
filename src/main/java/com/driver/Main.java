69883be746f9c6576e1f6a4b542c6ef3cb7d1f08
Swapnil <swapnilmahadule0597@gmail.com>
2023-03-15 17:02:13 +0530

    2nd commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.setName(10);
-        obj.getName();
+
+       // java: name has private access in com.driver.RWOnly
+        obj.setName("Swapnil");
+        String ans = obj.getName();
+        System.out.println(ans);
+
     }
 }
